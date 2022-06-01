c73685a56aa8ddcfde74721502a13bd6bb0b655d
Avery Yang <averyyang2674@gmail.com>
2022-06-01 11:13:50 -0500

    add audio

--- game.js
+++ game.js
@@ -1,1 +1,13 @@
-$('h1')
+var buttonColors = ['red', 'blue', 'green', 'yellow']
+var gamePattern = []
+
+function nextSequence() {
+    var randomNumber = ~~(Math.random() * 4); // 0-3
+    var randomChosenColor = buttonColors[randomNumber]
+    gamePattern.push(randomChosenColor);
+    //animate flash based on randomChosenColor
+    $(`#${randomChosenColor}`).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
+    //play sound based on buttonColors
+    var audio = new Audio(`sounds/${randomChosenColor}.mp3`);
+    audio.play();
+}
