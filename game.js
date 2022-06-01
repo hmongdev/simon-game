2384c958401370975647fc6b539b4316f18ee2a6
Avery Yang <averyyang2674@gmail.com>
2022-06-01 11:46:41 -0500

    check which button was pressed

--- game.js
+++ game.js
@@ -1,13 +1,25 @@
 var buttonColors = ['red', 'blue', 'green', 'yellow']
 var gamePattern = []
+var userClickedPattern = []
 
+//check which button was pressed
+$('.btn').click(function() {
+    var userChosenColor = $(this).attr('id');
+    userClickedPattern.push(userChosenColor);
+    console.log(userClickedPattern);
+});
+
+//create random color, animate, and play sound
 function nextSequence() {
+    //select random color, push it into gamePattern
     var randomNumber = ~~(Math.random() * 4); // 0-3
     var randomChosenColor = buttonColors[randomNumber]
     gamePattern.push(randomChosenColor);
-    //animate flash based on randomChosenColor
+    //print the gamePattern
+    console.log(gamePattern);
+    //animate flash based on random color
     $(`#${randomChosenColor}`).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
-    //play sound based on buttonColors
-    var audio = new Audio(`sounds/${randomChosenColor}.mp3`);
+    //play sound based on random color
+    var audio = new Audio(`sounds/${randomChosenColor}mp3`);
     audio.play();
 }
