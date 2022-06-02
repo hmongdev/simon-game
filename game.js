d2b2aea3a06c409b1d61e52aa3f198d217ded83f
Avery Yang <averyyang2674@gmail.com>
2022-06-01 20:29:06 -0500

    add touchstart event

--- game.js
+++ game.js
@@ -5,7 +5,7 @@ var level = 0;
 var started = false;
 
 //detect keboard press to begin game
-$(document).click(function () {
+$(document).touchstart(function () {
     if (!started) {
         //displays level #
         $('#level-title').text(`Level ${level}`);
