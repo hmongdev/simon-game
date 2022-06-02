3572df1946d515a49086658b63e2971b88765888
Avery Yang <averyyang2674@gmail.com>
2022-06-01 20:46:50 -0500

    add on tap

--- game.js
+++ game.js
@@ -5,7 +5,7 @@ var level = 0;
 var started = false;
 
 //detect keboard press to begin game
-$(document).on('tap', function () {
+$(document).on('touchstart', function () {
     if (!started) {
         //displays level #
         $('#level-title').text(`Level ${level}`);
